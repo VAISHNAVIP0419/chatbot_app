fa644ea345bab6fbc69ccc3587485bc0ff99f268
VAISHNAVIP0419 <134695731+VAISHNAVIP0419@users.noreply.github.com>
2025-04-17 13:56:03 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -95,6 +95,6 @@ resource "aws_security_group_rule" "allow_jenkins_to_eks" {
   to_port                  = 443
   protocol                 = "tcp"
   security_group_id        = aws_eks_cluster.eks.vpc_config[0].cluster_security_group_id
-  source_security_group_id = "YOUR_JENKINS_EC2_SECURITY_GROUP_ID"  # Replace this with your Jenkins EC2 SG ID
+  source_security_group_id = "sg-008f6ca898dda1bd2"  # Replace this with your Jenkins EC2 SG ID
 }
 
